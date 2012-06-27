5c21e17bd50547ced9460e3d92967356cc595c44
Pierre Chanial <pierre.chanial@cea.fr>
2012-06-27 20:27:21 +0200

    Add back test suite.

--- tests/test_wcsutils.py
+++ tests/test_wcsutils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from kapteyn import wcs
 from numpy.testing import assert_almost_equal
-from tamasis.utils import all_eq
+from pysimulators.utils import all_eq
 from pysimulators.wcsutils import (
     angle_lonlat,
     barycenter_lonlat,
