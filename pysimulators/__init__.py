bf9b495f92a8a4dd29e4c173c6b4862c2b9133b5
Pierre Chanial <pierre.chanial@cea.fr>
2012-09-18 12:03:46 +0200

    Add geometry.py file.

--- pysimulators/__init__.py
+++ pysimulators/__init__.py
@@ -1,6 +1,7 @@
 from pyoperators.utils.mpi import MPI
 
 from . import _wcsutils
+from . import geometry
 from .config import VERSION as __version__
 from .acquisitionmodels import *
 from .datatypes import *
