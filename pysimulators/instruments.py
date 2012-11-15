cc2adf53ff6c820a68242080a90d5c1b966f133f
Pierre Chanial <pierre.chanial@cea.fr>
2012-11-15 08:16:24 +0100

    Allow plot keywords in instrument's plot method.

--- pysimulators/instruments.py
+++ pysimulators/instruments.py
@@ -671,7 +671,7 @@ class Instrument(object):
         )
         return out
 
-    def plot(self, transform=None, autoscale=True):
+    def plot(self, transform=None, autoscale=True, **keywords):
         """
         Plot instrument footprint.
 
@@ -711,9 +711,15 @@ class Instrument(object):
         if self.nvertices > 0:
             patches = coords.reshape((-1,) + coords.shape[-2:])
             for p in patches:
-                a.add_patch(pyplot.Polygon(p, closed=True, fill=False))
+                a.add_patch(pyplot.Polygon(p, closed=True, fill=False, **keywords))
         else:
-            pyplot.plot(coords[..., 0], coords[..., 1], 'o')
+            if 'color' not in keywords:
+                keywords['color'] = 'black'
+            if 'marker' not in keywords:
+                keywords['marker'] = 'o'
+            if 'linestyle' not in keywords:
+                keywords['linestyle'] = ''
+            pyplot.plot(coords[..., 0], coords[..., 1], **keywords)
 
         if autoscale:
             pyplot.autoscale()
