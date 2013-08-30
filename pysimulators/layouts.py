0b76862458c923ac9029448ab566207ad75d9d9b
Pierre Chanial <pierre.chanial@gmail.com>
2013-08-30 19:34:36 +0200

    Format input layout shape as soon as possible.

--- pysimulators/layouts.py
+++ pysimulators/layouts.py
@@ -111,6 +111,7 @@ class Layout(object):
         a call to this function.
 
         """
+        shape = tointtuple(shape)
         removed = np.array(removed, dtype=bool)
         if not isscalar(removed) and removed.shape != shape:
             raise ValueError('Invalid shape of the removed attribute.')
@@ -123,7 +124,7 @@ class Layout(object):
         self._special_attributes = set()
         self._reserved_attributes = ('ndim', 'shape', 'nvertices', 'removed',
                                      'packed')
-        self.shape = tointtuple(shape)
+        self.shape = shape
         self.ndim = len(self.shape)
         self.removed = removed
         self.packed = _Packed(self)
