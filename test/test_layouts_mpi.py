ccb8f24b81066432f67adc8b2f24ae53c02bda2d
Pierre Chanial <pierre.chanial@gmail.com>
2014-05-26 01:02:13 +0200

    MPI-fied Layout.

---
+++ test/test_layouts_mpi.py
@@ -0,0 +1,20 @@
+from __future__ import division
+import numpy as np
+from pyoperators import MPI
+from pyoperators.utils import split
+from pyoperators.utils.testing import assert_same
+from pysimulators import Layout
+
+rank = MPI.COMM_WORLD.rank
+size = MPI.COMM_WORLD.size
+
+
+def test():
+    np.random.seed(0)
+    n = 4
+    x = np.random.random(n)
+    layout = Layout(n, x=x)
+    s = split(n, size, rank)
+    scattered = layout.scatter()
+    assert_same(scattered.x, x[s])
+    assert_same(scattered.all.x, x)
