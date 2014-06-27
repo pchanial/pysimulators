656be35755c7280edef90751f3ab79912d1778a7
Pierre Chanial <pierre.chanial@gmail.com>
2014-06-27 11:56:58 +0200

    Use FSRMatrix sparse storage for all projection-related operations.

--- test/test_scenegrid.py
+++ test/test_scenegrid.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import numpy as np
+from numpy.testing import assert_equal
 from pyoperators.utils import product
 from pyoperators.utils.testing import assert_same
 from pysimulators import Quantity, create_fitsheader, SceneGrid
@@ -30,17 +31,22 @@ def test_spatial_integration():
     )
     plane = SceneGrid.fromfits(header)
 
-    def func(center, npps, expected):
+    def func(center, ncolmax, expected):
         detectors = create_grid_squares((2, 2), Quantity(pixsize, 'um'), center=center)
-        proj = plane.get_integration_operator(detectors)
-        x = np.arange(product(plane_shape), dtype=float).reshape(plane_shape)
+        proj = plane.get_integration_operator(plane.topixel(detectors), ncolmax=ncolmax)
+        x = np.arange(len(plane)).reshape(plane_shape)
         y = proj(x)
 
-        assert proj.matrix.shape == (2, 2, npps)
+        assert_equal(proj.shapein, plane.shape)
+        assert_equal(proj.shapeout, detectors.shape[:-2])
+        assert_equal(proj.matrix.shape, (4, len(plane)))
+        expected_min_ncolmax = 1 if center == (0, 0) else 4
+        assert_equal(proj.matrix.data.shape, (4, max(expected_min_ncolmax, ncolmax)))
+        assert_equal(proj.min_ncolmax, expected_min_ncolmax)
         assert_same(y, expected)
 
     centers = [(0, 0), (-0.5, 0.5)]
-    nppss = (1, 4)
+    ncolmaxs = (0, 1, 4, 5)
     expecteds = [
         [[9, 10], [5, 6]],
         [
@@ -48,5 +54,6 @@ def test_spatial_integration():
             [0.25 * (4 + 5 + 8 + 9), 0.25 * (5 + 6 + 9 + 10)],
         ],
     ]
-    for center, npps, expected in zip(centers, nppss, expecteds):
-        yield func, center, npps, expected
+    for center, expected in zip(centers, expecteds):
+        for ncolmax in ncolmaxs:
+            yield func, center, ncolmax, expected
