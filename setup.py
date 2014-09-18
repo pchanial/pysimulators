0b2d80243ffbb18d937e78d5c673337a694e7cd8
Pierre Chanial <pierre.chanial@gmail.com>
2014-09-18 12:07:55 +0200

    Implement projection's restriction in Fortran, to avoid large temporaries.

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ if any(c in sys.argv for c in ('build', 'build_ext')):
     # write f2py's type mapping file
     root = os.path.dirname(__file__)
     with open(os.path.join(root, '.f2py_f2cmap'), 'w') as f:
-        f.write("{'real': {'sp': 'float', 'dp': 'double', 'p': 'double'},"
+        f.write("{'integer': {'int8': 'char', 'int16': 'short', 'int32': 'int', 'int64': 'long_long'},"
+                " 'real': {'sp': 'float', 'dp': 'double', 'p': 'double', 'real32': 'float', 'real64': 'double'},"
                 " 'complex': {'sp': 'complex', 'dp': 'complex_double', 'p': 'complex_double'}}\n")
 
 
